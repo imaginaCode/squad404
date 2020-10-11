e05eae0aeca122b66e4c53039dcc72b21e30901d
imaginaCode <imaginedreams.bfs@gmail.com>
2020-10-11 14:49:45 -0300

    maquina de estado

--- E-commerce/E-commerce/src/Main/Projeto.java
+++ E-commerce/E-commerce/src/Main/Projeto.java
@@ -8,36 +8,211 @@ public class Projeto {
 
 	public static char sexoIn;
 
-	public static Scanner leia = new Scanner(System.in);;
+	public static Scanner leia = new Scanner(System.in);
 
 	public static int cod[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-			quantEstoque[] = { 16, 10, 30, 25, 29, 12, 18, 22, 26, 11 },			
+			quantEstoque[] = { 16, 10, 30, 25, 29, 12, 18, 22, 26, 11 },
+			parcelaIn = 0,
 			 quantIn[] = new int[10];
+			 
 	
 	public static double valorUnitario[] = { 86, 90, 40, 35, 50, 60, 32, 30, 50, 63 },
 			valorTotal[] = new double[10],
 			valorTotalCarrinho, impostoTotalCarrinho,
+			DescontoAvista = 0,
 			valorTaxa[] = new double[10];
+	
+	public static boolean carrinho = false,
+			coletaNome = false;
+			
+			
 
 	public static String produtoNome[] = { "PythonMan", "Else Kruger", "Noite dos Errors", "O Jorro",
 			"Tech Leader América", "Dev de Ferro", "Capitão Cod", "Java Snow", "For Fantástico", "Capitã Main" };
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int menuprincipalIn= 1;
+		int navegadorInternoIn = 0;
+		int navIntCarrinhoIn = 0;
+		while(true) {
+			if(coletaNome == false)
+			{
+				nomeGet();
+				imprimirNome();
+			}
+			else 
+			{
+				linha();
+				if(menuprincipalIn == 0) break;
+				System.out.println("Menu Principal: Digite 1 - para comprar / 2 para carrinho / 0 - para sair ");
+				menuprincipalIn = leia.nextInt();
+				if(menuprincipalIn == 0) break;
 				
-		
-		
+				
+				if (menuprincipalIn == 1) // menu de compra
+				{
+					navegadorInternoIn = 1;
+					
+					while (true) {
+						if(navegadorInternoIn == 0) break;
+						linha();
+						System.out.println("Menu Compra");
+						compra();
+						while(true) {
+							System.out.println("deseja comprar mais produtos 1 - sim / 0 - nao");
+							navegadorInternoIn = leia.nextInt();
+							if(navegadorInternoIn == 0 || navegadorInternoIn == 1) break;
+							
+						}
+					}
+				}
+				
+				if (menuprincipalIn == 2) // menu de carrinho
+				{
+					navegadorInternoIn = 1;
+					navIntCarrinhoIn = 0;
+					while (true) {
+						if(navegadorInternoIn == 0) break;
+						linha();
+						System.out.println("Menu Carrinho");
+						checarCarrinho();
+						if(carrinho) {
+							while (true) {
+							System.out.println("Digite: \n 1 - finalizar compra \n 2 - editar carrinho \n 0 - para menu principal \n Leitura :");
+							navIntCarrinhoIn = leia.nextInt();
+							if (navIntCarrinhoIn == 1 || navIntCarrinhoIn == 2  || navIntCarrinhoIn == 0) break;
+							System.out.println("Opção invalida");
+							}
+							
+							if(navIntCarrinhoIn == 1) // menu finalizar compra
+							{
+								System.out.println("Função compra");
+								
+								navegadorInternoIn =0;
+								navIntCarrinhoIn = 0;
+								System.out.println("Compra finalizada");
+								linha();
+								linha();
+								while (true) {
+									System.out.println("Novo usuario ? \n1 - sim \n0 - sair do programa  ");
+									navegadorInternoIn = leia.nextInt();
+									if (navegadorInternoIn == 0 || navegadorInternoIn == 1) break;
+									
+								}
+								if(navegadorInternoIn == 1)
+								{
+									novoUsuario();
+									navegadorInternoIn = 0;
+									menuprincipalIn =1;
+									navIntCarrinhoIn = 0;
+									
+								} 
+								else if (navegadorInternoIn == 0) 
+								{
+									menuprincipalIn =0;
+									navegadorInternoIn = 0;
+									navIntCarrinhoIn = 0;
+									break;
+								}
+								
+								
+								
+							}
+							else if (navIntCarrinhoIn == 2) // menu editar carrinho
+							{
+								while (true) {
+									removerItemCarrinho();
+									
+									while (true)
+									{
+										System.out.println("Retirar outro item: 1 - sim / 0 - nao :");
+										navegadorInternoIn = leia.nextInt();
+										if (navegadorInternoIn == 0) break;
+										System.out.println("Opção invalida");
+									}
+									
+									if (navegadorInternoIn == 0) break;
+								}
+								
+								
+							} 
+							
+							else // escape
+							{
+								navegadorInternoIn =0;
+								
+							}
+							
+							
+							
+						} else {
+							while(true) {
+								System.out.println("Carrinho Vazio digite 0 para voltar ao menu principal");
+								navegadorInternoIn = leia.nextInt();
+								if(navegadorInternoIn == 0 ) break;
+								
+							}
+							
+						}
+					}
+					
+				}
+				
+				
+				
+			}
+			
+		}
 		
 		leia.close();
+		
+		
+		
 	}
+	
+	public static void novoUsuario() {
+		nomeIn = "";
+		sexoIn = ' ';
+		coletaNome = false;
+		DescontoAvista = 0;
+		parcelaIn = 0;
+		valorTotalCarrinho = 0;
+		impostoTotalCarrinho = 0;
+		for(int contador=0;contador<10;contador++) {
+			quantIn [contador]= 0;
+			valorTaxa [contador] = 0.0;
+			valorTotal [contador] = 0.0;
+			
+		}
+	}
+	
+	public static void checarCarrinho() {
+		carrinho = false;
+		for(int contador=0;contador<10;contador++) {
+			if (quantIn[contador]>0) carrinho =true;
+		}
+		
+	}
+	
+	public static void imprimirNome() {
+		if(sexoIn == 'H')System.out.printf("Bem vindo %s a nossa loja Cod In Fantasy \n",nomeIn);
+		if(sexoIn == 'M')System.out.printf("Bem vinda %s a nossa loja Cod In Fantasy \n",nomeIn);
+		if(sexoIn == 'O')System.out.printf("Bem vinde %s a nossa loja Cod In Fantasy \n",nomeIn);
+	}
+	
 
-	public static void nomeGet() {
+	public static void nomeGet() 
+	{
+		linha();
+		System.out.println("Cod In Fantasy store");
+		linha();
+		coletaNome = true;
 
-		System.out.print("Digite o seu nome : ");
+		System.out.print(" Por favor digite o seu nome : ");
 		nomeIn = leia.next();
 
-		System.out.println(nomeIn);
+		//System.out.println(nomeIn);
 		do {
 			System.out.print("Digite o seu sexo : H-homem, M-mulher, O-outros ");
 			sexoIn = leia.next().toUpperCase().charAt(0);
@@ -139,5 +314,55 @@ public class Projeto {
 		
 		imprimirCarrinho();
         }
+	
+	public static void removerItemCarrinho() {
+		 int podIn=0,qIn;	
+			
+			imprimirCarrinho();
+			linha();
+			do
+			{
+				System.out.print("Digite o código do produto que você deseja remover : ");
+				
+				podIn = leia.nextInt();
+				
+				if(podIn<0 || podIn>9)
+				{
+					System.out.println("Código inválido, digite novamente.");
+				}
+			
+			
+			}while(podIn<0 || podIn>9);
+			
+		
+			do
+			{
+		
+				System.out.printf("Produto : %s, foi selecionado. Digite a quantidade que deseja remover : ",produtoNome[podIn]);
+				qIn = leia.nextInt();
+				
+				if( (qIn>quantEstoque[podIn]) || qIn<=0)
+				{
+					System.out.println("Quantidade inválida, digite novamente.");
+				}
+				
+			}while((qIn>quantEstoque[podIn]) || qIn<=0);
+			
+			
+			quantIn[podIn] -= qIn;
+			
+			valorTotal[podIn]=(quantIn[podIn]* valorUnitario[podIn])*1.09;
+			
+			valorTaxa[podIn]=(quantIn[podIn]* valorUnitario[podIn])*0.09;
+			
+			quantEstoque[podIn]+=qIn;
+			
+			System.out.printf("Produto : %s\nQuantidade : %d\nREMOVIDO DO CARRINHO",produtoNome[podIn],quantIn[podIn]);
+			
+			System.out.println();
+			
+			imprimirCarrinho();
+		
+	}
 
 }
